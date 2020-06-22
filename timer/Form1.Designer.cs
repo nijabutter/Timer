82bedc17ae6e0a4629993c6a266ac33e917720bc
dilutedjuice <wongmcdonald@gmail.com>
2020-06-22 14:29:40 +0100

    On branch master  Initial commit  Changes to be committed: 	new file:   .vs/timer/v15/.suo 	new file:   .vs/timer/v15/Server/sqlite3/storage.ide 	new file:   .vs/timer/v15/Server/sqlite3/storage.ide-shm 	new file:   .vs/timer/v15/Server/sqlite3/storage.ide-wal 	new file:   timer.sln 	new file:   timer/App.config 	new file:   timer/Form1.Designer.cs 	new file:   timer/Form1.cs 	new file:   timer/Form1.resx 	new file:   timer/Program.cs 	new file:   timer/Properties/AssemblyInfo.cs 	new file:   timer/Properties/Resources.Designer.cs 	new file:   timer/Properties/Resources.resx 	new file:   timer/Properties/Settings.Designer.cs 	new file:   timer/Properties/Settings.settings 	new file:   timer/Resource1.Designer.cs 	new file:   timer/Resource1.resx 	new file:   timer/Resources/alarm.wav 	new file:   timer/bin/Debug/timer.exe 	new file:   timer/bin/Debug/timer.exe.config 	new file:   timer/bin/Debug/timer.pdb 	new file:   timer/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   timer/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   timer/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   timer/obj/Debug/timer.Form1.resources 	new file:   timer/obj/Debug/timer.Properties.Resources.resources 	new file:   timer/obj/Debug/timer.Resource1.resources 	new file:   timer/obj/Debug/timer.csproj.CoreCompileInputs.cache 	new file:   timer/obj/Debug/timer.csproj.FileListAbsolute.txt 	new file:   timer/obj/Debug/timer.csproj.GenerateResource.cache 	new file:   timer/obj/Debug/timer.csprojAssemblyReference.cache 	new file:   timer/obj/Debug/timer.exe 	new file:   timer/obj/Debug/timer.pdb 	new file:   timer/timer.csproj

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hours = new System.Windows.Forms.Label();
+            this.minutes = new System.Windows.Forms.Label();
+            this.seconds = new System.Windows.Forms.Label();
+            this.colon1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.increaseSeconds = new System.Windows.Forms.Button();
+            this.decreaseSeconds = new System.Windows.Forms.Button();
+            this.decreaseMinutes = new System.Windows.Forms.Button();
+            this.increaseMinutes = new System.Windows.Forms.Button();
+            this.decraseHours = new System.Windows.Forms.Button();
+            this.increaseHours = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.stop = new System.Windows.Forms.Button();
+            this.dark = new System.Windows.Forms.Label();
+            this.light = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.ontop = new System.Windows.Forms.CheckBox();
+            this.playsound = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hours
+            // 
+            this.hours.AutoSize = true;
+            this.hours.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hours.Location = new System.Drawing.Point(-4, 93);
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(104, 73);
+            this.hours.TabIndex = 0;
+            this.hours.Text = "00";
+            // 
+            // minutes
+            // 
+            this.minutes.AutoSize = true;
+            this.minutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutes.Location = new System.Drawing.Point(143, 93);
+            this.minutes.Name = "minutes";
+            this.minutes.Size = new System.Drawing.Size(104, 73);
+            this.minutes.TabIndex = 1;
+            this.minutes.Text = "00";
+            // 
+            // seconds
+            // 
+            this.seconds.AutoSize = true;
+            this.seconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seconds.Location = new System.Drawing.Point(290, 93);
+            this.seconds.Name = "seconds";
+            this.seconds.Size = new System.Drawing.Size(104, 73);
+            this.seconds.TabIndex = 2;
+            this.seconds.Text = "00";
+            // 
+            // colon1
+            // 
+            this.colon1.AutoSize = true;
+            this.colon1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colon1.Location = new System.Drawing.Point(97, 93);
+            this.colon1.Name = "colon1";
+            this.colon1.Size = new System.Drawing.Size(50, 73);
+            this.colon1.TabIndex = 3;
+            this.colon1.Text = ":";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 73);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":";
+            // 
+            // increaseSeconds
+            // 
+            this.increaseSeconds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.increaseSeconds.Location = new System.Drawing.Point(303, 67);
+            this.increaseSeconds.Name = "increaseSeconds";
+            this.increaseSeconds.Size = new System.Drawing.Size(75, 22);
+            this.increaseSeconds.TabIndex = 5;
+            this.increaseSeconds.Text = "↑";
+            this.increaseSeconds.UseVisualStyleBackColor = true;
+            this.increaseSeconds.Click += new System.EventHandler(this.increaseSeconds_Click);
+            // 
+            // decreaseSeconds
+            // 
+            this.decreaseSeconds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.decreaseSeconds.Location = new System.Drawing.Point(292, 180);
+            this.decreaseSeconds.Name = "decreaseSeconds";
+            this.decreaseSeconds.Size = new System.Drawing.Size(75, 23);
+            this.decreaseSeconds.TabIndex = 6;
+            this.decreaseSeconds.Text = "↓";
+            this.decreaseSeconds.UseVisualStyleBackColor = true;
+            this.decreaseSeconds.Click += new System.EventHandler(this.decreaseSeconds_Click);
+            // 
+            // decreaseMinutes
+            // 
+            this.decreaseMinutes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.decreaseMinutes.Location = new System.Drawing.Point(156, 180);
+            this.decreaseMinutes.Name = "decreaseMinutes";
+            this.decreaseMinutes.Size = new System.Drawing.Size(75, 23);
+            this.decreaseMinutes.TabIndex = 8;
+            this.decreaseMinutes.Text = "↓";
+            this.decreaseMinutes.UseVisualStyleBackColor = true;
+            this.decreaseMinutes.Click += new System.EventHandler(this.decreaseMinutes_Click);
+            // 
+            // increaseMinutes
+            // 
+            this.increaseMinutes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.increaseMinutes.Location = new System.Drawing.Point(156, 67);
+            this.increaseMinutes.Name = "increaseMinutes";
+            this.increaseMinutes.Size = new System.Drawing.Size(75, 23);
+            this.increaseMinutes.TabIndex = 7;
+            this.increaseMinutes.Text = "↑";
+            this.increaseMinutes.UseVisualStyleBackColor = true;
+            this.increaseMinutes.Click += new System.EventHandler(this.increaseMinutes_Click);
+            // 
+            // decraseHours
+            // 
+            this.decraseHours.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.decraseHours.Location = new System.Drawing.Point(7, 180);
+            this.decraseHours.Name = "decraseHours";
+            this.decraseHours.Size = new System.Drawing.Size(75, 23);
+            this.decraseHours.TabIndex = 10;
+            this.decraseHours.Text = "↓";
+            this.decraseHours.UseVisualStyleBackColor = true;
+            this.decraseHours.Click += new System.EventHandler(this.decraseHours_Click);
+            // 
+            // increaseHours
+            // 
+            this.increaseHours.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.increaseHours.Location = new System.Drawing.Point(7, 67);
+            this.increaseHours.Name = "increaseHours";
+            this.increaseHours.Size = new System.Drawing.Size(75, 23);
+            this.increaseHours.TabIndex = 9;
+            this.increaseHours.Text = "↑";
+            this.increaseHours.UseVisualStyleBackColor = true;
+            this.increaseHours.Click += new System.EventHandler(this.increaseHours_Click);
+            // 
+            // start
+            // 
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(292, 230);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(78, 51);
+            this.start.TabIndex = 11;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // stop
+            // 
+            this.stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(9, 230);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(78, 51);
+            this.stop.TabIndex = 12;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // dark
+            // 
+            this.dark.AutoSize = true;
+            this.dark.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dark.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dark.Location = new System.Drawing.Point(2, 3);
+            this.dark.Name = "dark";
+            this.dark.Size = new System.Drawing.Size(39, 29);
+            this.dark.TabIndex = 14;
+            this.dark.Text = "🌙";
+            this.dark.Click += new System.EventHandler(this.dark_Click);
+            // 
+            // light
+            // 
+            this.light.AutoSize = true;
+            this.light.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.light.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.light.Location = new System.Drawing.Point(47, 3);
+            this.light.Name = "light";
+            this.light.Size = new System.Drawing.Size(39, 29);
+            this.light.TabIndex = 15;
+            this.light.Text = "☀";
+            this.light.Click += new System.EventHandler(this.light_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(256, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(122, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // ontop
+            // 
+            this.ontop.AutoSize = true;
+            this.ontop.Location = new System.Drawing.Point(151, 230);
+            this.ontop.Name = "ontop";
+            this.ontop.Size = new System.Drawing.Size(92, 17);
+            this.ontop.TabIndex = 16;
+            this.ontop.Text = "Always on top";
+            this.ontop.UseVisualStyleBackColor = true;
+            this.ontop.CheckedChanged += new System.EventHandler(this.ontop_CheckedChanged);
+            // 
+            // playsound
+            // 
+            this.playsound.AutoSize = true;
+            this.playsound.Location = new System.Drawing.Point(151, 252);
+            this.playsound.Name = "playsound";
+            this.playsound.Size = new System.Drawing.Size(80, 17);
+            this.playsound.TabIndex = 17;
+            this.playsound.Text = "Play Sound";
+            this.playsound.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 295);
+            this.Controls.Add(this.playsound);
+            this.Controls.Add(this.ontop);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.light);
+            this.Controls.Add(this.dark);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.decraseHours);
+            this.Controls.Add(this.increaseHours);
+            this.Controls.Add(this.decreaseMinutes);
+            this.Controls.Add(this.increaseMinutes);
+            this.Controls.Add(this.decreaseSeconds);
+            this.Controls.Add(this.increaseSeconds);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colon1);
+            this.Controls.Add(this.seconds);
+            this.Controls.Add(this.minutes);
+            this.Controls.Add(this.hours);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(405, 334);
+            this.MinimumSize = new System.Drawing.Size(405, 334);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Timer";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hours;
+        private System.Windows.Forms.Label minutes;
+        private System.Windows.Forms.Label seconds;
+        private System.Windows.Forms.Label colon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button increaseSeconds;
+        private System.Windows.Forms.Button decreaseSeconds;
+        private System.Windows.Forms.Button decreaseMinutes;
+        private System.Windows.Forms.Button increaseMinutes;
+        private System.Windows.Forms.Button decraseHours;
+        private System.Windows.Forms.Button increaseHours;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Label dark;
+        private System.Windows.Forms.Label light;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox ontop;
+        private System.Windows.Forms.CheckBox playsound;
+    }
+}
+
---
+++ timer/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        public int iseconds;
+        public int iminutes;
+        public int ihours;
+        public int totalSeconds;
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+        
+        private void refreshS()
+        {
+            if (iseconds < 10)
+            {
+                seconds.Text = "0" + iseconds.ToString();
+                return;
+            }
+            seconds.Text = iseconds.ToString();
+        }
+
+        private void refreshM()
+        {
+            if (iminutes < 10)
+            {
+                minutes.Text = "0" + iminutes.ToString();
+                return;
+            }
+            minutes.Text = iminutes.ToString();
+        }
+
+        private void refreshH()
+        {
+            if (ihours < 10)
+            {
+                hours.Text = "0" + ihours.ToString();
+                return;
+            }
+            hours.Text = ihours.ToString();
+        }
+        private void increaseSeconds_Click(object sender, EventArgs e)
+        {
+            if (iseconds == 59)
+            {
+                iseconds = 0;
+                refreshS();
+                increaseMinutes_Click(sender, e);
+            }
+            iseconds++;
+            refreshS();
+        }
+        
+        private void increaseMinutes_Click(object sender, EventArgs e)
+        {
+            if (iminutes == 59)
+            {
+                iminutes = 0;
+                refreshM();
+                increaseHours_Click(sender, e);
+            }
+            iminutes++;
+            refreshM();
+        }
+
+        private void increaseHours_Click(object sender, EventArgs e)
+        {
+            ihours++;
+            refreshH();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            ToolTip tt = new ToolTip();
+            tt.AutoPopDelay = 2000;
+            tt.InitialDelay = 300;
+            tt.ReshowDelay = 500;
+            tt.ShowAlways = false;
+            tt.SetToolTip(dark, "Dark Mode");
+            tt.SetToolTip(light, "Light Mode");
+            iseconds = 0;
+            iminutes = 0;
+            ihours = 0;
+            seconds.Text = iseconds.ToString();
+            minutes.Text = iminutes.ToString();
+            hours.Text = ihours.ToString();
+            refreshS();
+            refreshM();
+            refreshH();
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\timer"))
+            {
+                using (StreamReader f = new StreamReader(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "timer")))
+                {
+                    String r = f.ReadLine();
+                    if (r == "dark")
+                    {
+                        f.Close();
+                        setDark();
+                    }
+                    else
+                    {
+                        f.Close();
+                        setLight();
+                    }
+                    
+                }
+            }
+            else
+            {
+                using (StreamWriter f = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "timer")))
+                {
+                    f.WriteLine("dark");
+                    f.Close();
+                    setDark();
+                }
+            }
+        }
+
+        private void decreaseSeconds_Click(object sender, EventArgs e)
+        {
+            if (!(iseconds == 0))
+            {
+                iseconds--;
+                refreshS();
+            }
+        }
+        private void setDark()
+        {
+            this.BackColor = Color.FromArgb(255, 31, 31, 31);
+            this.ForeColor = Color.White;
+            increaseHours.ForeColor = Color.Gray;
+            increaseMinutes.ForeColor = Color.Gray;
+            increaseSeconds.ForeColor = Color.Gray;
+            decraseHours.ForeColor = Color.Gray;
+            decreaseMinutes.ForeColor = Color.Gray;
+            decreaseSeconds.ForeColor = Color.Gray;
+            dark.ForeColor = Color.Gray;
+            light.ForeColor = Color.White;
+            using (StreamWriter f = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "timer")))
+                f.WriteLine("dark");
+        }
+
+        private void setLight()
+        {
+            this.BackColor = Color.FromName("Control");
+            this.ForeColor = Color.Black;
+            increaseHours.ForeColor = Color.Black;
+            increaseMinutes.ForeColor = Color.Black;
+            increaseSeconds.ForeColor = Color.Black;
+            decraseHours.ForeColor = Color.Black;
+            decreaseMinutes.ForeColor = Color.Black;
+            decreaseSeconds.ForeColor = Color.Black;
+            light.ForeColor = Color.Gray;
+            dark.ForeColor = Color.Black;
+            using (StreamWriter f = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "timer")))
+                f.WriteLine("light");
+        }
+        private void decreaseMinutes_Click(object sender, EventArgs e)
+        {
+            if (!(iminutes == 0))
+            {
+                iminutes--;
+                refreshM();
+            }
+        }
+
+        private void decraseHours_Click(object sender, EventArgs e)
+        {
+            if (!(ihours == 0))
+            {
+                ihours--;
+                refreshH();
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            this.Text = hours.Text + ":" + minutes.Text + ":" + seconds.Text;
+            start.Enabled = false;
+            stop.Enabled = true;
+            timer.Enabled = true;
+            increaseHours.Hide();
+            increaseMinutes.Hide();
+            increaseSeconds.Hide();
+            decraseHours.Hide();
+            decreaseSeconds.Hide();
+            decreaseMinutes.Hide();
+            decreaseMinutes.Hide();
+            totalSeconds = iseconds + (iminutes * 60) + (ihours * 3600);
+}
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            this.Text = "Timer";
+            start.Enabled = true;
+            stop.Enabled = false;
+            timer.Enabled = false;
+            increaseHours.Show();
+            increaseMinutes.Show();
+            increaseSeconds.Show();
+            decraseHours.Show();
+            decreaseSeconds.Show();
+            decreaseMinutes.Show();
+            decreaseMinutes.Show();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            
+            if (totalSeconds == 0)
+            {
+                timer.Stop();
+                stop_Click(sender, e);
+                if (playsound.Checked == true)
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resource1.alarm);
+                    player.Play();
+                }
+                return;
+            }
+            else if (iseconds > 0)
+            {
+                totalSeconds--;
+                iseconds -= 1;
+                //seconds.Text = iseconds.ToString();
+                refreshS();
+            }
+            else if (iminutes > 0)
+            {
+                iminutes -= 1;
+                iseconds = 59;
+                //minutes.Text = iminutes.ToString();
+                refreshS();
+                refreshM();
+            }
+            else if (ihours > 0)
+            {
+                ihours -= 1;
+                iminutes = 59;
+                iseconds = 59;
+                //hours.Text = ihours.ToString();
+                refreshS();
+                refreshM();
+                refreshH();
+            }
+            this.Text = hours.Text + ":" + minutes.Text + ":" + seconds.Text;
+        }
+
+        private void dark_Click(object sender, EventArgs e)
+        {
+            if (dark.ForeColor == Color.Gray)
+                return;
+            setDark();
+        }
+
+        private void light_Click(object sender, EventArgs e)
+        {
+            if (light.ForeColor == Color.Gray)
+                return;
+            setLight();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = ((double)trackBar1.Value / 100.0);
+
+        }
+
+        private void ontop_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ontop.Checked == true)
+            {
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = false;
+            }
+        }
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+        }
+    }
+}
